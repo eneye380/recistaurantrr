d927c18c1ce6acc7e237bf9bd8785e83708fb3ec
eneye380 <a.e.abdulkarim@rgu.ac.uk>
2016-04-10 22:17:14 +0200

    Update lib.php

--- lib.php
+++ lib.php
@@ -4,7 +4,8 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<title>ReciStaurant: Find Recipes, Find Nearby Restaurants</title>
+<!--title>ReciStaurant: Find Recipes, Find Nearby Restaurants</title-->
+<title>ReciStaurant: Find Nearby Restaurants</title>
         
         <link rel="stylesheet" href="lib/bootstrap-3.3.6-dist/bootstrap-3.3.6-dist/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
